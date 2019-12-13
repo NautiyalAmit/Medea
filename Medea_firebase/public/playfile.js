106a2a014faf1b4a34b0609784e6d96a2d502ef6
Tarun Bhatia <tarunbhatia256@gmail.com>
2019-12-13 01:32:09 +0100

    Responsive elements -updated UI

--- Medea_firebase/public/playfile.js
+++ Medea_firebase/public/playfile.js
@@ -1,17 +1,2 @@
-let element = document.createElement("div")
-element.innerHTML="<p> player to be imported here! </p>" // To be replaced by the player's code
-let divelement = document.getElementsByClassName("container")[0];
-console.log(divelement);
-let id_song = document.getElementsByClassName("d-block w-100")[0];
-console.log(id_song);
-id_song.addEventListener("click",function(e){
-
-    divelement.replaceWith(element);
-
-});
-
-
-// console.log(id_song[0]);
-// id_song.
-// let element = document.createElement("div")
-// element.innerHTML="<p> element replaced </p>"
+let id_song = document.getElementsByClassName("d-block w-100");
+console.log(id_song[0]);
